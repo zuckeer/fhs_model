2883d51e2dcc22e2566f0f500f9974987ad4ecf7
Marcos Alves <zuckeer@gmail.com>
2022-08-23 22:16:58 -0300

    feat: contif.toml adjust

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.streamlit/ 
-echo "\ [server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\" > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
